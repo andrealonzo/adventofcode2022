98cbe8b9a40a97e67bf6e59a4c84b88cc15ae570
Andre Alonzo <analonzo@cj.com>
2022-12-02 10:08:12 -0800

    calculate calories per elf and get the max

--- 1/calories.test.ts
+++ 1/calories.test.ts
@@ -6,4 +6,23 @@ test('most calories by elf by single elf', () => {
     const actualCalories = mostCaloriesThatAnElfIsCarrying(calorieInput);
     expect(actualCalories).toBe(expectedCalories)
 })
+test('most calories by elf between two elves', () => {
+    const expectedCalories = 1000
+    const calorieInput =
+        "1000\n" +
+        "\n" +
+        "500"
+    const actualCalories = mostCaloriesThatAnElfIsCarrying(calorieInput);
+    expect(actualCalories).toBe(expectedCalories)
+})
+
+test('add calories by elf by single elf', () => {
+    const expectedCalories = 1500
+    const calorieInput =
+        "1000\n" +
+        "500"
+    const actualCalories = mostCaloriesThatAnElfIsCarrying(calorieInput);
+    expect(actualCalories).toBe(expectedCalories)
+})
+
 
--- 1/calories.ts
+++ 1/calories.ts
@@ -1,3 +1,14 @@
-export const mostCaloriesThatAnElfIsCarrying = (caloriesInput:string) =>{
-    return 2000;
+function parseCaloriesInput(caloriesInput: string) {
+    const caloriesString = caloriesInput.split(/\n\n/)
+    let caloriesAdded = []
+    for (let i = 0; i < caloriesString.length; i++) {
+        let calorieSum = caloriesString[i].split(/\n/).map(Number).reduce((partialSum, a) => partialSum + a, 0);
+        caloriesAdded[i] = calorieSum;
+    }
+    return caloriesAdded
+}
+
+export const mostCaloriesThatAnElfIsCarrying = (caloriesInput:string):number =>{
+    const caloriesPerElf:number[] = parseCaloriesInput(caloriesInput);
+    return Math.max(...caloriesPerElf);
 }
