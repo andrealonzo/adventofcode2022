f1ea4f5c18dba522108e083e4b747b43b995ef4c
Andre Alonzo <analonzo@cj.com>
2022-12-02 15:56:59 -0800

    Day 2 - refactor

--- 2/rockPaperScissorsCalculator.test.ts
+++ 2/rockPaperScissorsCalculator.test.ts
@@ -1,4 +1,10 @@
-import {createCalculator} from './rockPaperScissorsCalculator'
+import {createCalculator, Strategy} from './rockPaperScissorsCalculator'
+
+const testRockPaperScissorsCalculator = (input:string, expectedScore, strategy: Strategy, ) => {
+    const rockPaperScissorsCalculator = createCalculator(input, strategy)
+    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
+    expect(actualScore).toBe(expectedScore)
+};
 
 test('calculate advent of code sample input total score', () => {
     const expectedScore = 15
@@ -6,88 +12,68 @@ test('calculate advent of code sample input total score', () => {
         "A Y\n" +
         "B X\n" +
         "C Z"
-    const rockPaperScissorsCalculator = createCalculator(input, 1)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.Original);
 })
 
 test('rock tie', () => {
     const expectedScore = 4
     const input =
         "A X"
-    const rockPaperScissorsCalculator = createCalculator(input, 1)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.Original);
 })
 
 test('paper tie', () => {
     const expectedScore = 5
     const input =
         "B Y"
-    const rockPaperScissorsCalculator = createCalculator(input, 1)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.Original);
 })
 
 test('scissors tie', () => {
     const expectedScore = 6
     const input =
         "C Z"
-    const rockPaperScissorsCalculator = createCalculator(input, 1)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.Original);
 })
 test('rock win', () => {
     const expectedScore = 7
     const input =
         "C X"
-    const rockPaperScissorsCalculator = createCalculator(input, 1)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.Original);
 })
 
 test('paper win', () => {
     const expectedScore = 8
     const input =
         "A Y"
-    const rockPaperScissorsCalculator = createCalculator(input, 1)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.Original);
 })
 
 test('scissors win', () => {
     const expectedScore = 9
     const input =
         "B Z"
-    const rockPaperScissorsCalculator = createCalculator(input, 1)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.Original);
 })
 test('rock loses', () => {
     const expectedScore = 1
     const input =
         "B X"
-    const rockPaperScissorsCalculator = createCalculator(input, 1)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.Original);
 })
 
 test('paper loses', () => {
     const expectedScore = 2
     const input =
         "C Y"
-    const rockPaperScissorsCalculator = createCalculator(input, 1)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.Original);
 })
 
 test('scissors loses', () => {
     const expectedScore = 3
     const input =
         "A Z"
-    const rockPaperScissorsCalculator = createCalculator(input, 1)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.Original);
 })
 
 test('calculate AoC sample input total score with strategy 2', () => {
@@ -96,83 +82,63 @@ test('calculate AoC sample input total score with strategy 2', () => {
         "A Y\n" +
         "B X\n" +
         "C Z"
-    const rockPaperScissorsCalculator = createCalculator(input, 2)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.New);
 })
 
 test('rock tie strategy 2', () => {
     const expectedScore = 4
     const input =
         "A Y"
-    const rockPaperScissorsCalculator = createCalculator(input, 2)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.New);
 })
 test('paper tie strategy 2', () => {
     const expectedScore = 5
     const input =
         "B Y"
-    const rockPaperScissorsCalculator = createCalculator(input, 2)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.New);
 })
 test('scissors tie strategy 2', () => {
     const expectedScore = 6
     const input =
         "C Y"
-    const rockPaperScissorsCalculator = createCalculator(input, 2)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.New);
 })
 
 test('rock win strategy 2', () => {
     const expectedScore = 8
     const input =
         "A Z"
-    const rockPaperScissorsCalculator = createCalculator(input, 2)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.New);
 })
 test('paper win strategy 2', () => {
     const expectedScore = 9
     const input =
         "B Z"
-    const rockPaperScissorsCalculator = createCalculator(input, 2)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.New);
 })
 test('scissors win strategy 2', () => {
     const expectedScore = 7
     const input =
         "C Z"
-    const rockPaperScissorsCalculator = createCalculator(input, 2)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.New);
 })
 test('rock lose strategy 2', () => {
     const expectedScore = 3
     const input =
         "A X"
-    const rockPaperScissorsCalculator = createCalculator(input, 2)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.New);
 })
 test('paper lose strategy 2', () => {
     const expectedScore = 1
     const input =
         "B X"
-    const rockPaperScissorsCalculator = createCalculator(input, 2)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.New);
 })
 test('scissors lose strategy 2', () => {
     const expectedScore = 2
     const input =
         "C X"
-    const rockPaperScissorsCalculator = createCalculator(input, 2)
-    const actualScore = rockPaperScissorsCalculator.calculateTotalScore();
-    expect(actualScore).toBe(expectedScore)
+    testRockPaperScissorsCalculator(input, expectedScore, Strategy.New);
 })
 
 
--- 2/rockPaperScissorsCalculator.ts
+++ 2/rockPaperScissorsCalculator.ts
@@ -1,84 +1,120 @@
 interface RockPaperScissorsCalculator {
     calculateTotalScore: () => number
 }
+
 interface Round {
-    p1: string,
-    p2: string
+    p1: Shape,
+    p2: string | Outcome | Shape
 }
+
+enum Shape {
+    Rock = 'A',
+    Paper = 'B',
+    Scissors = 'C',
+    Undefined = '',
+}
+
+enum Outcome {
+    Lose = 'X',
+    Draw = 'Y',
+    Win = 'Z'
+}
+
+enum RoundScore {
+    Win = 6,
+    Lose = 0,
+    Draw = 3
+}
+
+export enum Strategy {
+    Original = 1,
+    New = 2
+}
+
+const shapeScores = new Map([
+    [Shape.Rock, 1],
+    [Shape.Paper, 2],
+    [Shape.Scissors, 3],
+]);
+
+
 export const createCalculator = (input: string, strategy: number): RockPaperScissorsCalculator => {
     const parseRounds = (input: string): Round[] => {
         const rounds: string[] = input.split(/\n/)
         return rounds.map((roundStr) => {
             const roundArr = roundStr.split(" ")
             const round: Round = {
-                p1: roundArr[0],
+                p1: <Shape>roundArr[0],
                 p2: roundArr[1]
             };
             return round
         })
     }
 
-    const selectedShapeScore = (shape: string): number => {
-        if (shape === 'X') {
-            return 1
-        } else if (shape === 'Y') {
-            return 2
-        } else if (shape === 'Z') {
-            return 3
-        }
-        return 0
+    const selectedShapeScore = (shape: Shape): number => {
+        return shapeScores.get(shape) || 0
     };
 
-    const isTie = (round: Round): boolean => {
-        return (round.p1 === 'A' && round.p2 === 'X') || (round.p1 === 'B' && round.p2 === 'Y') || (round.p1 === 'C' && round.p2 === 'Z')
+    const isDraw = (round: Round): boolean => {
+        return (round.p1 === Shape.Rock && round.p2 === Shape.Rock) ||
+            (round.p1 === Shape.Paper && round.p2 === Shape.Paper) ||
+            (round.p1 === Shape.Scissors && round.p2 === Shape.Scissors)
     };
 
-    const isWon = (round: Round): boolean => {
-        return (round.p1 === 'C' && round.p2 === 'X') || (round.p1 === 'A' && round.p2 === 'Y') || (round.p1 === 'B' && round.p2 === 'Z')
+    const isWin = (round: Round): boolean => {
+        return (round.p1 === Shape.Scissors && round.p2 === Shape.Rock) ||
+            (round.p1 === Shape.Rock && round.p2 === Shape.Paper) ||
+            (round.p1 === Shape.Paper && round.p2 === Shape.Scissors)
     };
 
-    const outcomeOfRoundScore = (round: Round): number => {
-        if (isTie(round)) {
-            return 3
-        } else if (isWon(round)) {
-            return 6
+    const outcomeOfRoundScore = (round: Round): RoundScore => {
+        if (isDraw(round)) {
+            return RoundScore.Draw
+        } else if (isWin(round)) {
+            return RoundScore.Win
         }
-        //loss
-        return 0
+        return RoundScore.Lose
     };
 
-    function chooseSelectedShape(round: Round): string {
-        if(round.p2 ==='Y'){
-            if(round.p1 ==='A')
-                return 'X'
-            if(round.p1 ==='B')
-                return 'Y'
-            if(round.p1 ==='C')
-                return 'Z'
+    function chooseSelectedShape(round: Round, strategy: Strategy): Shape {
+        if (strategy === Strategy.New) {
+            if (round.p2 === Outcome.Draw) {
+                if (round.p1 === Shape.Rock)
+                    return Shape.Rock
+                if (round.p1 === Shape.Paper)
+                    return Shape.Paper
+                if (round.p1 === Shape.Scissors)
+                    return Shape.Scissors
+            } else if (round.p2 === Outcome.Win) {
+                if (round.p1 === Shape.Rock)
+                    return Shape.Paper
+                if (round.p1 === Shape.Paper)
+                    return Shape.Scissors
+                if (round.p1 === Shape.Scissors)
+                    return Shape.Rock
+            } else if (round.p2 === Outcome.Lose) {
+                if (round.p1 === Shape.Rock)
+                    return Shape.Scissors
+                if (round.p1 === Shape.Paper)
+                    return Shape.Rock
+                if (round.p1 === Shape.Scissors)
+                    return Shape.Paper
+            }
+        } else if (strategy === Strategy.Original) {
+            if (round.p2 === 'X') {
+                return Shape.Rock
+            } else if (round.p2 === 'Y') {
+                return Shape.Paper
+            } else if (round.p2 === 'Z') {
+                return Shape.Scissors
+            }
         }
-        else if(round.p2 ==='Z'){
-            if(round.p1 ==='A')
-                return 'Y'
-            if(round.p1 ==='B')
-                return 'Z'
-            if(round.p1 ==='C')
-                return 'X'
-        } else if(round.p2 ==='X'){
-            if(round.p1 ==='A')
-                return 'Z'
-            if(round.p1 ==='B')
-                return 'X'
-            if(round.p1 ==='C')
-                return 'Y'
-        }
-        return ''
+        return Shape.Undefined
     }
 
     const getRoundScore = (round: Round): number => {
-        if(strategy === 2){
-            round.p2 = chooseSelectedShape(round)
-        }
-        return selectedShapeScore(round.p2) + outcomeOfRoundScore(round)
+        round.p2 = chooseSelectedShape(round, strategy)
+        return selectedShapeScore(<Shape>round.p2)  + outcomeOfRoundScore(round)
     }
 
     const getTotalScore = (rounds: Round[]): number => {
