8151e881cc4ba8ec77eb2ea92ada1ce9d2513253
Andre Alonzo <analonzo@cj.com>
2022-12-04 15:40:57 -0800

    Day 4 - refactor

--- 4/challenge.ts
+++ 4/challenge.ts
@@ -13,16 +13,14 @@ export const createCalculator = (input: string): Challenge => {
 
     const parseInput = (input: string): PairAssignments[] => {
         const assignments: string[] = input.split(/\n/)
-        let pairAssignments: PairAssignments[] = [];
-        for (let i = 0; i < assignments.length; i++) {
-            const pairs: string[] = assignments[i].split(",")
+        return assignments.map((assignment)=>{
+            const pairs: string[] = assignment.split(",")
             const pairAssignment: PairAssignments = {
                 pair1: pairs[0].split("-").map(Number),
                 pair2: pairs[1].split("-").map(Number),
             }
-            pairAssignments.push(pairAssignment)
-        }
-        return pairAssignments
+            return pairAssignment
+        })
     }
 
     const pairAssignments: PairAssignments[] = parseInput(input)
@@ -32,13 +30,9 @@ export const createCalculator = (input: string): Challenge => {
     }
 
     const calculateAnswer1 = () => {
-        let numFullyContained = 0;
-        for (let i = 0; i < pairAssignments.length; i++) {
-            if (isFullyContainedIn(pairAssignments[i].pair1, pairAssignments[i].pair2)) {
-                numFullyContained++;
-            }
-        }
-        return numFullyContained
+        return pairAssignments
+            .filter((pairAssignment)=>isFullyContainedIn(pairAssignment.pair1, pairAssignment.pair2))
+            .length
     };
 
     function isOverlapping(pair1: number[], pair2: number[]) {
@@ -46,13 +40,9 @@ export const createCalculator = (input: string): Challenge => {
     }
 
     const calculateAnswer2 = () => {
-        let numOverlap = 0;
-        for (let i = 0; i < pairAssignments.length; i++) {
-            if (isOverlapping(pairAssignments[i].pair1, pairAssignments[i].pair2)) {
-                numOverlap++;
-            }
-        }
-        return numOverlap
+        return pairAssignments
+            .filter((pairAssignment)=>isOverlapping(pairAssignment.pair1, pairAssignment.pair2))
+            .length
     };
 
 
