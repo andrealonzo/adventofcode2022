766a706f5bf637c019b381d55f0474c679d61e30
Andre Alonzo <analonzo@cj.com>
2022-12-07 12:46:36 -0800

    Day 7 - solved part 1

--- 2022/7/challenge.test.ts
+++ 2022/7/challenge.test.ts
@@ -1,11 +1,228 @@
 import {createCalculator} from './challenge'
 
-test('sample test', () => {
-    const expected = 1
-    const input = "6-6,4-6"
+test('parse small', () => {
+    const input = "$ cd /\n" +
+        "$ ls\n" +
+        "dir a\n" +
+        "14848514 b.txt\n" +
+        "8504156 c.dat\n" +
+        "dir d"
+
+    const expected = "- / (dir)\n" +
+        "   - b.txt (file, size=14848514)\n" +
+        "   - c.dat (file, size=8504156)\n" +
+        "  - a (dir)\n" +
+        "  - d (dir)\n"
     const calculator = createCalculator(input)
-    const actual = calculator.calculateAnswer1();
-    expect(actual).toBe(expected)
+    const actual = calculator.printTree()
+    expect(actual).toStrictEqual(expected)
 })
 
+test('parse medium', () => {
+    const input = "$ cd /\n" +
+        "$ ls\n" +
+        "dir a\n" +
+        "14848514 b.txt\n" +
+        "8504156 c.dat\n" +
+        "dir d\n" +
+        "$ cd a\n" +
+        "$ ls\n" +
+        "dir e\n" +
+        "29116 f\n" +
+        "2557 g\n" +
+        "62596 h.lst"
 
+
+    const expected = "- / (dir)\n" +
+        "   - b.txt (file, size=14848514)\n" +
+        "   - c.dat (file, size=8504156)\n" +
+        "  - a (dir)\n" +
+        "     - f (file, size=29116)\n" +
+        "     - g (file, size=2557)\n" +
+        "     - h.lst (file, size=62596)\n" +
+        "    - e (dir)\n" +
+        "  - d (dir)\n"
+    const calculator = createCalculator(input)
+    const actual = calculator.printTree()
+    expect(actual).toStrictEqual(expected)
+})
+
+test('parse mediumish', () => {
+    const input = "$ cd /\n" +
+        "$ ls\n" +
+        "dir a\n" +
+        "14848514 b.txt\n" +
+        "8504156 c.dat\n" +
+        "dir d\n" +
+        "$ cd a\n" +
+        "$ ls\n" +
+        "dir e\n" +
+        "29116 f\n" +
+        "2557 g\n" +
+        "62596 h.lst\n" +
+        "$ cd e\n" +
+        "$ ls\n" +
+        "584 i"
+
+
+    const expected = "- / (dir)\n" +
+        "   - b.txt (file, size=14848514)\n" +
+        "   - c.dat (file, size=8504156)\n" +
+        "  - a (dir)\n" +
+        "     - f (file, size=29116)\n" +
+        "     - g (file, size=2557)\n" +
+        "     - h.lst (file, size=62596)\n" +
+        "    - e (dir)\n" +
+        "       - i (file, size=584)\n" +
+        "  - d (dir)\n"
+    const calculator = createCalculator(input)
+    const actual = calculator.printTree()
+    expect(actual).toStrictEqual(expected)
+})
+
+test('parse full sample 1', () => {
+    const input = "$ cd /\n" +
+        "$ ls\n" +
+        "dir a\n" +
+        "14848514 b.txt\n" +
+        "8504156 c.dat\n" +
+        "dir d\n" +
+        "$ cd a\n" +
+        "$ ls\n" +
+        "dir e\n" +
+        "29116 f\n" +
+        "2557 g\n" +
+        "62596 h.lst\n" +
+        "$ cd e\n" +
+        "$ ls\n" +
+        "584 i\n" +
+        "$ cd ..\n" +
+        "$ cd ..\n" +
+        "$ cd d\n" +
+        "$ ls\n" +
+        "4060174 j\n" +
+        "8033020 d.log\n" +
+        "5626152 d.ext\n" +
+        "7214296 k"
+    const expected = "- / (dir)\n" +
+        "   - b.txt (file, size=14848514)\n" +
+        "   - c.dat (file, size=8504156)\n" +
+        "  - a (dir)\n" +
+        "     - f (file, size=29116)\n" +
+        "     - g (file, size=2557)\n" +
+        "     - h.lst (file, size=62596)\n" +
+        "    - e (dir)\n" +
+        "       - i (file, size=584)\n" +
+        "  - d (dir)\n" +
+        "     - j (file, size=4060174)\n" +
+        "     - d.log (file, size=8033020)\n" +
+        "     - d.ext (file, size=5626152)\n" +
+        "     - k (file, size=7214296)\n"
+    const calculator = createCalculator(input)
+    const actual = calculator.printTree()
+    expect(actual).toStrictEqual(expected)
+})
+
+test('get directory sizes small', () => {
+    const input = "$ cd /\n" +
+        "$ ls\n" +
+        "dir a\n" +
+        "14848514 b.txt\n" +
+        "8504156 c.dat\n" +
+        "dir d"
+
+    const expected = [
+        {
+            "name": "a",
+            "size": 0
+        },
+        {
+            "name": "d",
+            "size": 0
+        },
+        {
+            "name": "/",
+            "size": 23352670
+        }
+    ]
+    const calculator = createCalculator(input)
+    const actual = calculator.getDirectorySizes()
+    expect(actual).toStrictEqual(expected)
+})
+test('get directory sizes full', () => {
+    const input = "$ cd /\n" +
+        "$ ls\n" +
+        "dir a\n" +
+        "14848514 b.txt\n" +
+        "8504156 c.dat\n" +
+        "dir d\n" +
+        "$ cd a\n" +
+        "$ ls\n" +
+        "dir e\n" +
+        "29116 f\n" +
+        "2557 g\n" +
+        "62596 h.lst\n" +
+        "$ cd e\n" +
+        "$ ls\n" +
+        "584 i\n" +
+        "$ cd ..\n" +
+        "$ cd ..\n" +
+        "$ cd d\n" +
+        "$ ls\n" +
+        "4060174 j\n" +
+        "8033020 d.log\n" +
+        "5626152 d.ext\n" +
+        "7214296 k"
+
+    const expected = [
+        {
+            "name": "e",
+            "size": 584
+        },
+        {
+            "name": "a",
+            "size": 94853
+        },
+        {
+            "name": "d",
+            "size": 24933642
+        },
+        {
+            "name": "/",
+            "size": 48381165
+        }
+    ]
+    const calculator = createCalculator(input)
+    const actual = calculator.getDirectorySizes()
+    expect(actual).toStrictEqual(expected)
+})
+test('total size of directories at most 100000', () => {
+    const input = "$ cd /\n" +
+        "$ ls\n" +
+        "dir a\n" +
+        "14848514 b.txt\n" +
+        "8504156 c.dat\n" +
+        "dir d\n" +
+        "$ cd a\n" +
+        "$ ls\n" +
+        "dir e\n" +
+        "29116 f\n" +
+        "2557 g\n" +
+        "62596 h.lst\n" +
+        "$ cd e\n" +
+        "$ ls\n" +
+        "584 i\n" +
+        "$ cd ..\n" +
+        "$ cd ..\n" +
+        "$ cd d\n" +
+        "$ ls\n" +
+        "4060174 j\n" +
+        "8033020 d.log\n" +
+        "5626152 d.ext\n" +
+        "7214296 k"
+
+    const expected = 95437
+    const calculator = createCalculator(input)
+    const actual = calculator.totalSizeOfDirectoriesAtMost(100000)
+    expect(actual).toStrictEqual(expected)
+})
--- 2022/7/challenge.ts
+++ 2022/7/challenge.ts
@@ -1,18 +1,145 @@
+export interface Directory {
+    parent: Directory | undefined;
+    name: string,
+    contents: DirectoryContents
+}
+
+interface DirectoryFile {
+    name: string
+    size: number
+}
+
+interface DirectoryContents {
+    directories: Directory[],
+    files: DirectoryFile[]
+}
+
 export const createCalculator = (input: string): any => {
-    const parseInput = (input: string): any => {
+    function parseCommands() {
+        return input.split(/\n/)
     }
 
+    const rootDirectory: Directory = {
+        name: "/",
+        parent: undefined,
+        contents: {
+            directories: [],
+            files: []
+        }
+    }
+
+    const parseInput = (): any => {
+
+        const commands = parseCommands()
+        let curNode = rootDirectory
+        for (let i = 0; i < commands.length; i++) {
+            const commandParams = commands[i].split(" ");
+            if (commandParams[0] === "$" && commandParams[1] === "cd") {
+                if (commandParams[2] === "/") {
+                    curNode = rootDirectory
+                } else if (commandParams[2] === "..") {
+                    //@ts-ignore
+                    curNode = curNode.parent
+                } else {
+                    for (let j = 0; j < curNode.contents.directories.length; j++) {
+                        const curDir = curNode.contents.directories[j]
+                        if (curDir.name === commandParams[2]) {
+                            curNode = curDir
+                            break
+                        }
+                    }
+                }
+            } else if (commandParams[0] === "$" && commandParams[1] === "ls") {
+                //don't do anything?
+            } else if (commandParams[0] === "dir") {
+                const newDir: Directory = {
+                    name: commandParams[1],
+                    parent: curNode,
+                    contents: {
+                        directories: [],
+                        files: []
+                    }
+                }
+                curNode.contents.directories.push(newDir)
+            } else {
+                //list of files
+                const newFile: DirectoryFile = {
+                    name: commandParams[1],
+                    size: parseInt(commandParams[0])
+                }
+                curNode.contents.files.push(newFile)
+            }
+        }
+        return rootDirectory
+    }
+    const treeToString = (node: Directory, spaces: string): string => {
+        let str = spaces + "- " + node.name + " (dir)\n"
+        for (let i = 0; i < node.contents.files.length; i++) {
+
+            str += spaces + "   - " + node.contents.files[i].name + " (file, size=" + node.contents.files[i].size + ")\n"
+        }
+        for (let i = 0; i < node.contents.directories.length; i++) {
+            str += treeToString(node.contents.directories[i], spaces + "  ")
+        }
+        return str
+    }
+    parseInput()
     const calculateAnswer1 = () => {
+        return totalSizeOfDirectoriesAtMost(100000)
     };
 
 
     const calculateAnswer2 = () => {
     };
+    const printTree = () => {
+        return treeToString(rootDirectory, "")
+    }
 
+    //@ts-ignore
+    let directorySizes= []
+    //@ts-ignore
+    const getDirectorySize = (directory):number => {
+        let directorySize = 0
+        console.log("calculating directory size for ", directory.name, directorySize)
+        console.log("directory files for ", directory.name, directory.contents.files)
+        for (let i = 0; i < directory.contents.files.length; i++) {
+            directorySize +=  directory.contents.files[i].size
+        }
+        console.log("after files calculating directory size for ", directory.name, directorySize)
+        for (let i = 0; i < directory.contents.directories.length; i++) {
+            directorySize += getDirectorySize(directory.contents.directories[i])
+        }
+        console.log("directory size", directory.name, directorySize)
+        directorySizes.push({
+            name:directory.name,
+            size:directorySize
+        })
+        return directorySize
+
+    }
+    const getDirectorySizes = () =>{
+        console.log(printTree())
+        getDirectorySize(rootDirectory)
+        //@ts-ignore
+        return directorySizes
+    }
+
+    const totalSizeOfDirectoriesAtMost = (num:number) =>{
+        const directorySizes  = getDirectorySizes()
+        return directorySizes
+            .map((directorySize)=>directorySize.size)
+            .filter((directorySize)=>directorySize<=num)
+            .reduce((prev,cur)=>prev+cur)
+    }
 
     return {
         calculateAnswer1: calculateAnswer1,
         calculateAnswer2: calculateAnswer2,
+        parseInput,
+        printTree,
+        getDirectorySize,
+        getDirectorySizes,
+        totalSizeOfDirectoriesAtMost
     }
 
 }
