c9774d77a23d749ae19fd56a28610cf3ba900255
Andre Alonzo <analonzo@cj.com>
2022-12-14 10:29:27 -0800

    2022 day 14 part 1 and 2 refactor

--- 2022/14/challenge.ts
+++ 2022/14/challenge.ts
@@ -14,10 +14,8 @@ export const createCalculator = (input: string): any => {
         let max = 0
         for (let i = 0; i < commands.length; i++) {
             for (let j = 0; j < commands[i].length; j++) {
-
                 if (commands[i][j][0] > max)
                     max = commands[i][j][0]
-
             }
         }
         return max + 1
@@ -38,14 +36,7 @@ export const createCalculator = (input: string): any => {
         return max + 2
     }
 
-    const parseInput = (input: string): any => {
-        const commands = input.split(/\n/).map((line) => line.trim().split(" -> ").map((coord) => coord.split(",").map(Number)))
-        const colSize = getMaxCol(commands)
-        const rowSize = getMaxRow(commands)
-        let grid = new Array(rowSize)
-        for (let i = 0; i < rowSize; i++) {
-            grid[i] = new Array(colSize).fill(".");
-        }
+    function fillGrid(commands: number[][][], grid: any[]) {
         for (let i = 0; i < commands.length; i++) {
             for (let j = 1; j < commands[i].length; j++) {
                 const start = commands[i][j - 1]
@@ -69,40 +60,29 @@ export const createCalculator = (input: string): any => {
         }
         return grid
     }
+
+    const parseInput = (input: string): any => {
+        const commands = input.split(/\n/).map((line) => line.trim().split(" -> ").map((coord) => coord.split(",").map(Number)))
+        const colSize = getMaxCol(commands)
+        const rowSize = getMaxRow(commands)
+        let grid = new Array(rowSize)
+        for (let i = 0; i < rowSize; i++) {
+            grid[i] = new Array(colSize).fill(".");
+        }
+        return fillGrid(commands, grid);
+    }
     const parseInput2 = (input: string): any => {
         const commands: number[][][] = input.split(/\n/).map((line) => line.trim().split(" -> ").map((coord) => coord.split(",").map(Number)))
-        const colSize = getMaxCol(commands) *2
+        const colSize = getMaxCol(commands) * 2
         const rowSize = getMaxRow(commands)
-        // commands.push([[0,rowSize],[colSize,rowSize]])
         commands.push([[0, rowSize], [colSize, rowSize]])
-        // console.log("commands",commands)
 
         let grid = new Array(rowSize + 2)
         for (let i = 0; i < rowSize + 2; i++) {
             grid[i] = new Array(colSize).fill(".");
         }
-        for (let i = 0; i < commands.length; i++) {
-            for (let j = 1; j < commands[i].length; j++) {
-                const start = commands[i][j - 1]
-                const end = commands[i][j]
-                if (start[0] == end[0]) {
-                    const r = start[0]
-                    const startC = start[1] < end[1] ? start[1] : end[1]
-                    const endC = start[1] > end[1] ? start[1] : end[1]
-                    for (let k = startC; k <= endC; k++) {
-                        grid[k][r] = "#"
-                    }
-                } else {
-                    const c = start[1]
-                    const startR = start[0] < end[0] ? start[0] : end[0]
-                    const endR = start[0] > end[0] ? start[0] : end[0]
-                    for (let k = startR; k <= endR; k++) {
-                        grid[c][k] = "#"
-                    }
-                }
-            }
-        }
-        return grid
+
+        return fillGrid(commands, grid);
     }
 
     function notAtRest(sand: number[], grid: string[][]) {
@@ -138,65 +118,38 @@ export const createCalculator = (input: string): any => {
     function startSand(grid: string[][]) {
         let sand = [0, 500]
         grid[sand[0]][sand[1]] = "o"
-        // console.log(printGrid(grid))
         let numSand = 0
-
         try {
 
             while (true) {
                 while (notAtRest(sand, grid)) {
                     sand = continueFall(sand, grid)
                 }
-                // console.log(printGrid(grid))
+
                 sand = [0, 500]
                 numSand++
-                // console.log(numSand)
             }
 
         } catch (err) {
             return numSand
         }
-        return numSand
-        // grid[sand[0]][sand[100]]  = "o"
-
-
-        // if()
-
-
     }
 
     function startSand2(grid: string[][]) {
         let sand = [0, 500]
         grid[sand[0]][sand[1]] = "o"
-        // console.log(printGrid(grid))
         let numSand = 0
-
-        try {
-
-            while (true) {
-                while (notAtRest(sand, grid)) {
-                    sand = continueFall(sand, grid)
-                }
-                if (grid[0][500] === "o") {
-                    return numSand
-                }
-                // console.log(printGrid(grid))
-                sand = [0, 500]
-                grid[sand[0]][sand[1]] = "o"
-                numSand++
-                // console.log(numSand)
+        while (true) {
+            while (notAtRest(sand, grid)) {
+                sand = continueFall(sand, grid)
             }
-
-        } catch (err) {
-            return numSand
+            if (grid[0][500] === "o") {
+                return numSand
+            }
+            sand = [0, 500]
+            grid[sand[0]][sand[1]] = "o"
+            numSand++
         }
-        return numSand
-        // grid[sand[0]][sand[100]]  = "o"
-
-
-        // if()
-
-
     }
 
     const calculateAnswer1 = () => {
