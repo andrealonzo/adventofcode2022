0ec1454e5e73bcb8402c7e85a688a2debb40fe36
Andre Alonzo <analonzo@cj.com>
2022-12-15 16:12:59 -0800

    2022 day 15 part 2

--- 2022/15/challenge.ts
+++ 2022/15/challenge.ts
@@ -9,18 +9,6 @@ interface SensorBeacon {
     distance?: number
 }
 
-//
-// function printGrid(grid: any) {
-//     let str = ""
-//     for (let i = 0; i < grid.length; i++) {
-//         for (let j = 0; j < grid[i].length; j++) {
-//             str += grid[i][j]
-//         }
-//         str += "\n"
-//     }
-//     return str
-// }
-
 export const createCalculator = (input: string): any => {
     let rOffset
     let cOffset
@@ -46,138 +34,6 @@ export const createCalculator = (input: string): any => {
             }
         )
     }
-    //
-    // function applyOffsets(sensorBeacons: SensorBeacon[], rOffset: number, cOffset: number) {
-    //     sensorBeacons.forEach((sensorBeacon) => {
-    //         sensorBeacon.sensor.c += cOffset
-    //         sensorBeacon.sensor.r += rOffset
-    //         sensorBeacon.beacon.c += cOffset
-    //         sensorBeacon.beacon.r += rOffset
-    //     })
-    //     return sensorBeacons
-    // }
-    //
-    // function createGrid(sensorBeacons: SensorBeacon[], rSize, cSize): string[][] {
-    //
-    //     //
-    //     // const cMin = sensorBeacons.reduce((prevMin, curBeacon) => {
-    //     //     if (curBeacon.beacon.c < prevMin) {
-    //     //         return curBeacon.beacon.c
-    //     //     } else if (curBeacon.sensor.c < prevMin) {
-    //     //         return curBeacon.sensor.c
-    //     //     }
-    //     //     return prevMin
-    //     // }, 10000000000)
-    //     // const cMax = sensorBeacons.reduce((prevMax, curBeacon) => {
-    //     //     if (curBeacon.beacon.c > prevMax) {
-    //     //         return curBeacon.beacon.c
-    //     //     } else if (curBeacon.sensor.c > prevMax) {
-    //     //         return curBeacon.sensor.c
-    //     //     }
-    //     //     return prevMax
-    //     // }, 0)
-    //     // const rMin = sensorBeacons.reduce((prevMin, curBeacon) => {
-    //     //     if (curBeacon.beacon.r < prevMin) {
-    //     //         return curBeacon.beacon.r
-    //     //     } else if (curBeacon.sensor.r < prevMin) {
-    //     //         return curBeacon.sensor.r
-    //     //     }
-    //     //     return prevMin
-    //     // }, 100000000000)
-    //     // const rMax = sensorBeacons.reduce((prevMax, curBeacon) => {
-    //     //     if (curBeacon.beacon.r > prevMax) {
-    //     //         return curBeacon.beacon.r
-    //     //     } else if (curBeacon.sensor.r > prevMax) {
-    //     //         return curBeacon.sensor.r
-    //     //     }
-    //     //     return prevMax
-    //     // }, 0)
-    //     // rOffset = rMin < 0 ? -rMin : 0
-    //     // cOffset = cMin < 0 ? -cMin : 0
-    //     // const rSize = rMax - rMin + 1
-    //     // const cSize = cMax - cMin + 1
-    //     sensorBeacons = applyOffsets(sensorBeacons, rOffset, cOffset)
-    //     // console.log("gridsize", rSize, cSize)
-    //     const grid = new Array(rSize)
-    //     for (let i = 0; i < grid.length; i++) {
-    //         grid[i] = new Array(cSize).fill(".")
-    //     }
-    //     // console.log("before initializing grid")
-    //     // const grid = new Array(rSize).fill(null).map(() => new Array(cSize).fill(null));
-    //     // console.log("after initializing grid")
-    //
-    //     sensorBeacons.forEach((sb) => {
-    //         // console.log(sb)
-    //         // console.log(cOffset)
-    //         grid[sb.sensor.r][sb.sensor.c] = "S"
-    //         grid[sb.beacon.r][sb.beacon.c] = "B"
-    //     })
-    //     return grid
-    //     // console.log(printGrid(grid))
-    //
-    // }
-    //
-    // function displaySensorCoverage(sb: SensorBeacon, grid: string[][]): string[][] {
-    //     // console.log(sb)
-    //     const distance = Math.abs(sb.sensor.c - sb.beacon.c) + Math.abs(sb.sensor.r - sb.beacon.r)
-    //     // console.log("distance", distance)
-    //     let colsToPrintOnEachSide = 0
-    //     // console.log(distance)
-    //     for (let r = sb.sensor.r - distance; r <= sb.sensor.r + distance; r++) {
-    //         // console.log(r,rOffset,sb.sensor.c,cOffset)
-    //         // console.log(grid[r+rOffset][sb.sensor.c+cOffset]
-    //         // print middle
-    //         if (r >= 0 && r < grid.length) {
-    //             // console.log("start",sb.sensor.c - colsToPrintOnEachSide, sb.sensor.c + colsToPrintOnEachSide)
-    //             for (let c = sb.sensor.c - colsToPrintOnEachSide; c <= sb.sensor.c + colsToPrintOnEachSide; c++) {
-    //                 // console.log("grid[r + rOffset][c + cOffset]",grid[r + rOffset][c + cOffset])
-    //                 if (grid[r][c] === ".") {
-    //                     grid[r][c] = "#"
-    //                 }
-    //             }
-    //
-    //             // if (grid[r + rOffset][sb.sensor.c + cOffset] === ".") {
-    //             //     grid[r + rOffset][sb.sensor.c + cOffset] = "#"
-    //             // }
-    //         }
-    //
-    //         if (r < sb.sensor.r) {
-    //             colsToPrintOnEachSide++
-    //         } else {
-    //             colsToPrintOnEachSide--
-    //         }
-    //     }
-    //     return grid
-    //
-    // }
-
-
-    // function displayCoverage(sensorBeacons: SensorBeacon[], grid: string[][]): string[][] {
-    //
-    //     // grid = displaySensorCoverage(sensorBeacons[6], grid)
-    //     // console.log(printGrid(grid))
-    //     sensorBeacons.forEach((sb) => {
-    //         grid = displaySensorCoverage(sb, grid)
-    //         // console.log(printGrid(grid))
-    //     })
-    //     return grid
-    //
-    // }
-    //
-    // function sensorsCloseToRow(sensorBeacons: SensorBeacon[], row: number) {
-    //     return sensorBeacons.filter((sensorBeacon) => {
-    //         const distanceFromSensorToRow = Math.abs(sensorBeacon.sensor.r - row)
-    //         // console.log("distanceFromSensorToRow",distanceFromSensorToRow)
-    //         return sensorBeacon.distance >= distanceFromSensorToRow
-    //     })
-    // }
-
-    // function updateSensorBeaconDistances(sensorBeacons: SensorBeacon[]) {
-    //     sensorBeacons.forEach((sb) => {
-    //         sb.distance = Math.abs(sb.sensor.c - sb.beacon.c) + Math.abs(sb.sensor.r - sb.beacon.r)
-    //     })
-    //     return sensorBeacons;
-    // }
 
     function getGridSize(sensorBeacons: SensorBeacon[]): { rMin: number, rMax: number, cMin: number, cMax: number, rSize: number, cSize: number } {
 
@@ -228,17 +84,12 @@ export const createCalculator = (input: string): any => {
     function withinAnySensorDistance(row: number, c: number, sensorBeacons: SensorBeacon[]): boolean {
         for (let i = 0; i < sensorBeacons.length; i++) {
             const sensorBeacon = sensorBeacons[i]
-            // console.log(row,c,sensorBeacon)
             const sensorToPointDistance = getDistance(row, c, sensorBeacon.sensor.r, sensorBeacon.sensor.c)
-            // console.log("sensorToPointDistance",sensorToPointDistance)
-            // console.log("sensorBeacon.distance",sensorBeacon.distance)
             // @ts-ignore
             if (sensorToPointDistance <= sensorBeacon.distance) {
-                // console.log("true")
                 return true
             }
         }
-        // console.log("false")
         return false;
     }
 
@@ -254,10 +105,7 @@ export const createCalculator = (input: string): any => {
                 numBeaconsInRow++
             }
         })
-        console.log(numBeaconsInRow)
         return numBeaconsInRow
-
-
     }
 
     const calculateAnswer1 = (r = 2000000) => {
@@ -275,67 +123,42 @@ export const createCalculator = (input: string): any => {
         return numNoBeacon - numBeaconsInRow
     };
 
-    // const calculateAnswer1 = (row = 2000000) => {
-    // let sensorBeacons = parseInput(input)
-    // sensorBeacons = updateSensorBeaconDistances(sensorBeacons);
-    // console.log("sensorBeacons", sensorBeacons)
-    // const closeSensors = sensorsCloseToRow(sensorBeacons, row)
-    // console.log("closeSensors", closeSensors)
-    //
-    // const originalGridSize = getGridSize(sensorBeacons)
-    // const closeSensorsGridSize = getGridSize(closeSensors)
-    // console.log("originalGridSize",originalGridSize)
-    // console.log("closeSensorsGridSize",closeSensorsGridSize)
-    //
-    // let grid = createGrid(closeSensors, closeSensorsGridSize.rSize, originalGridSize.cSize)
-    // // let grid = createGrid(sensorBeacons)
-    //
-    // console.log(printGrid(grid))
-    //
-    // grid = displayCoverage(sensorBeacons, grid)
-    // console.log(printGrid(grid))
-    // // // console.log(row-rOffset-2)
-    //  const count = grid[row].filter((item) => item === "#").length
-    //  return count
-    // // return 1
-    // };
-
-
-    function findDistressBeacon(maxSize: number, sensorBeacons: SensorBeacon[]):Coord {
+    function findDistressBeacon(maxSize: number, sensorBeacons: SensorBeacon[]): Coord {
         for (let r = 0; r < maxSize; r++) {
-            let numNoBeacon = 0
             for (let c = 0; c < maxSize; c++) {
-                if (withinAnySensorDistance(r, c, sensorBeacons)) {
-                    numNoBeacon++
-                } else {
-                    return {
-                        r, c
+                let numSensorsHit = 0
+                for (let sensorNum = 0; sensorNum < sensorBeacons.length; sensorNum++) {
+                    const sensorBeacon = sensorBeacons[sensorNum]
+                    const sensorToPointDistance = getDistance(r, c, sensorBeacon.sensor.r, sensorBeacon.sensor.c)
+                    // @ts-ignore
+                    if (sensorToPointDistance <= sensorBeacon.distance) {
+                        numSensorsHit++
+                        if (c < sensorBeacon.sensor.c) {
+                            c = Math.min((sensorBeacon.sensor.c - c) + sensorBeacon.sensor.c, maxSize)
+                        } else {
+                            //@ts-ignore
+                            c = c + sensorBeacon.distance - sensorToPointDistance
+                        }
                     }
                 }
+                if (numSensorsHit === 0) {
+                    return {r, c}
+                }
             }
-            if(r%100===0){
-                console.log("done with",r,"/",maxSize)
+            if (r % 1000 === 0) {
+                console.log("done with", r, "/", maxSize)
             }
-            // console.log("numNoBeacon", numNoBeacon)
         }
-        return{
-            r:0,
-            c:0
+        return {
+            r: 0,
+            c: 0
         }
-
     }
 
     const calculateAnswer2 = (maxSize: number = 4000000) => {
         let sensorBeacons = parseInput(input)
-        // const gridSize = getGridSize(sensorBeacons)
-        // const colStart = gridSize.cMin*20
-        // const colEnd = gridSize.cMax*20
         const distressBeacon = findDistressBeacon(maxSize, sensorBeacons)
-
         return distressBeacon.c * 4000000 + distressBeacon.r
-        // const numBeaconsInRow = findNumBeaconsInRow(sensorBeacons, r)
-        // return numNoBeacon - numBeaconsInRow
-
     };
 
 
