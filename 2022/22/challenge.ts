a361deef9ff81cf99e7897f0e2514356858d1c30
Andre Alonzo <analonzo@cj.com>
2022-12-22 15:28:48 -0800

    day 22 part 1

--- 2022/22/challenge.ts
+++ 2022/22/challenge.ts
@@ -1,22 +1,7 @@
 export const createCalculator = (input: string): any => {
-    const parseInput = (input: string): any => {
-
+    const parseInput = (input: string): { instructions: string[], map: string[][] } => {
         const [mapStr, instStr] = input.split(/\n\n/)
-        const mapRowsStr = mapStr.split(/\n/)
-        const solidWalls: Set<string> = new Set()
-        const openTiles: Set<string> = new Set()
-        for (let i = 0; i < mapRowsStr.length; i++) {
-            const mapRowStr = mapRowsStr[i]
-            for (let j = 0; j < mapRowStr.length; j++) {
-                const char = mapRowStr.charAt(j)
-                if (char == ".") {
-                    openTiles.add(JSON.stringify([i, j]))
-                } else if (char == "#") {
-                    solidWalls.add(JSON.stringify([i, j]))
-                }
-            }
-        }
-        const start = [0, mapRowsStr[0].indexOf(".")]
+        let map: string[][] = mapStr.split(/\n/).map((row) => row.split(""))
         let instructions = []
         let curNum = ""
         let char = ""
@@ -31,116 +16,111 @@ export const createCalculator = (input: string): any => {
             }
         }
         instructions.push(char)
-        console.log(instructions)
-        return [
-            start,
-            solidWalls,
-            openTiles,
+        return {
             instructions,
-            mapRowsStr.length
-        ]
-    }
-
-    function moveForward(curPos: number[], curDirection: number[], solidWalls: Set<string>, openTiles: Set<string>): number[] {
-        let newPos = [curPos[0] + curDirection[0], curPos[1] + curDirection[1]]
-        return newPos
-    }
-
-    function moveBackwards(curPos: number[], curDirection: number[], solidWalls: Set<string>, openTiles: Set<string>): number[] {
-        let newPos = [curPos[0] - curDirection[0], curPos[1] - curDirection[1]]
-        return newPos
-    }
-
-    function loopAround(curPos: number[], curDirection: number[], solidWalls: Set<string>, openTiles: Set<string>): number[] {
-        //keep going backwards until you hit empty space again?
-        // curPos = moveBackwards(curPos, curDirection, solidWalls, openTiles)
-        while (solidWalls.has(JSON.stringify(curPos)) || openTiles.has(JSON.stringify(curPos))) {
-
-            curPos = moveBackwards(curPos, curDirection, solidWalls, openTiles)
+            map,
         }
-        return moveForward(curPos, curDirection, solidWalls, openTiles)
+    }
+
+    function moveForward(curPos: { r: number, c: number }, curDirection: number[]): { r: number, c: number } {
+        return {r: curPos.r + curDirection[0], c: curPos.c + curDirection[1]}
+    }
+
+    function moveBackwards(curPos: { r: number, c: number }, curDirection: number[]): { r: number, c: number } {
+        return {r: curPos.r - curDirection[0], c: curPos.c - curDirection[1]}
+    }
+
+
+    function loopAround(curPos: { r: number, c: number }, curDirection: number[], map: string[][]): { r: number, c: number } {
+        //keep going backwards until you hit empty space again
+        while ((map[curPos.r] !== undefined && map[curPos.r][curPos.c] !== undefined) && map[curPos.r][curPos.c] !== " ") {
+            curPos = moveBackwards(curPos, curDirection)
+        }
+        return moveForward(curPos, curDirection)
+    }
+
+    function getStartingPos(curPos: { r: number; c: number }, map: string[][]) {
+        while (map[0][curPos.c] == " ") {
+            curPos.c = curPos.c + 1
+        }
+        return curPos
+    }
+
+    const right = [0, 1]
+    const left = [0, -1]
+    const up = [-1, 0]
+    const down = [1, 0]
+
+    function getNewDirection(curDirection: number[], instruction: string): number[] {
+
+        if (instruction == "L") {
+            if (curDirection == right) {
+                curDirection = up
+            } else if (curDirection == up) {
+                curDirection = left
+            } else if (curDirection == left) {
+                curDirection = down
+            } else if (curDirection == down) {
+                curDirection = right
+            }
+        } else if (instruction == "R") {
+            if (curDirection == right) {
+                curDirection = down
+            } else if (curDirection == down) {
+                curDirection = left
+            } else if (curDirection == left) {
+                curDirection = up
+            } else if (curDirection == up) {
+                curDirection = right
+            }
+        }
+        return curDirection;
     }
 
     const calculateAnswer1 = () => {
-            const [start, solidWalls, openTiles, instructions, numRows] = parseInput(input)
-            let curPos = start
-            const right = [0, 1]
-            const left = [0, -1]
-            const up = [-1, 0]
-            const down = [1, 0]
-            let curDirection = right
-            instructions.forEach((instruction: string) => {
-                console.log("instruction", instruction, "curPos", curPos)
-                if (instruction == "L") {
-                    if (curDirection == right) {
-                        curDirection = up
-                    } else if (curDirection == up) {
-                        curDirection = left
-                    } else if (curDirection == left) {
-                        curDirection = down
-                    } else if (curDirection == down) {
-                        curDirection = right
-                    }
-                } else if (instruction == "R") {
-                    if (curDirection == right) {
-                        curDirection = down
-                    } else if (curDirection == down) {
-                        curDirection = left
-                    } else if (curDirection == left) {
-                        curDirection = up
-                    } else if (curDirection == up) {
-                        curDirection = right
-                    }
-                } else {
-                    const numMoves = parseInt(instruction)
-                    for (let i = 0; i < numMoves; i++) {
-                        curPos = moveForward(curPos, curDirection, solidWalls, openTiles )
-                        //if hit wall, move backwards
-                        if (solidWalls.has(JSON.stringify(curPos))) {
-                            curPos = moveBackwards(curPos, curDirection,  solidWalls, openTiles)
-                            continue
-                        }
-                        else if (!solidWalls.has(JSON.stringify(curPos)) && !openTiles.has(JSON.stringify(curPos))) {
-                            console.log("out of bounds")
-                            curPos = moveBackwards(curPos, curDirection,  solidWalls, openTiles)
-                            const loopAroundPos = loopAround(curPos, curDirection, solidWalls, openTiles)
-                            if(!solidWalls.has(JSON.stringify(curPos))){
 
-                                curPos = loopAroundPos
-                            }
-                            console.log("curPos after looparound", curPos)
-                            //if out of bounds,circle around
+        const {instructions, map} = parseInput(input)
+        let curDirection = right
+        let curPos = {
+            r: 0,
+            c: 0
+        }
+        curPos = getStartingPos(curPos, map)
 
+        instructions.forEach((instruction: string) => {
+            if (instruction == "L" || instruction == "R") {
+                curDirection = getNewDirection(curDirection, instruction)
+            } else {
+                const numMoves = parseInt(instruction)
+                for (let i = 0; i < numMoves; i++) {
+                    curPos = moveForward(curPos, curDirection)
+                    if (map[curPos.r] == undefined || map[curPos.r][curPos.c] == undefined || map[curPos.r][curPos.c] == " ") {
+                        curPos = moveBackwards(curPos, curDirection)
+                        const loopAroundPos = loopAround(curPos, curDirection, map)
+                        if (map[loopAroundPos.r][loopAroundPos.c] !== "#") {
+                            curPos = loopAroundPos
                         }
+                    } else if (map[curPos.r][curPos.c] === "#") {
+                        curPos = moveBackwards(curPos, curDirection)
                     }
                 }
-            })
-            console.log("final Position", curPos)
-            let facingScore = 0
-            if (curDirection == down) {
-                facingScore = 1
-            } else if (curDirection == left) {
-                facingScore = 2
-            } else if (curDirection == up) {
-                facingScore = 3
             }
-
-         const row = curPos[0] + 1
-            // const row = numRows -curPos[0]
-            const col = curPos[1]+1
-            const answer = (1000 * row)+ (4 * col) + facingScore
-        console.log("final Position", row, col, facingScore, curDirection)
-
-        console.log("final answer", answer)
-        return answer
-            // return 6032
+        })
+        let facingScore = 0
+        if (curDirection == down) {
+            facingScore = 1
+        } else if (curDirection == left) {
+            facingScore = 2
+        } else if (curDirection == up) {
+            facingScore = 3
         }
-    ;
-
+        const row = curPos.r + 1
+        const col = curPos.c + 1
+        return (1000 * row) + (4 * col) + facingScore
+    }
 
     const calculateAnswer2 = () => {
     };
-
 
     return {
         calculateAnswer1: calculateAnswer1,
